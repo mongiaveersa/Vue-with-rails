8866d48baa5bee2a790d3e9a64561a2a3dcb9d4d
Shubham Mongia <shubham@veersatech.com>
2021-10-08 10:39:06 +0530

    First

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'welcome#index'
+
+  resources :users
+  get 'app', to: 'users#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
